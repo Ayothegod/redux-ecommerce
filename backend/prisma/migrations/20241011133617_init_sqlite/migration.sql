c439236d9d755959e20f89909f444351ea91a575
Ayothegod <heyayomideadebisi@gmail.com>
2024-10-11 15:47:48 +0100

    convert to sqlite and add cors permissions

---
+++ backend/prisma/migrations/20241011133617_init_sqlite/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "accountType" TEXT NOT NULL DEFAULT 'SHOPPER',
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "imageUrl" TEXT,
+    "category" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "sellerId" TEXT NOT NULL,
+    CONSTRAINT "Product_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ProductTag" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    CONSTRAINT "ProductTag_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "totalAmount" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "shopperId" TEXT NOT NULL,
+    CONSTRAINT "Order_shopperId_fkey" FOREIGN KEY ("shopperId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "orderId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "sellerId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "OrderItem_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,22 @@ import { Hono } from "hono";
 import authRoute from "./routes/auth.route.js";
 import sellerProductsRoute from "./routes/products.route.js";
 import productsOrders from "./routes/orders.route.js";
+import { cors } from "hono/cors";
 
-const hello = "";
 const app = new Hono();
+
+app.use("/api/*", cors());
+app.use(
+  "/api2/*",
+  cors({
+    origin: "http://example.com",
+    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
 app.get("/", (c) => {
   return c.text("Hello User, Welcome to paxx.");
@@ -17,7 +30,11 @@ app.route("/api/v1/orders", productsOrders);
 const port = 6000;
 console.log(`Server is running on port ${port}`);
 
-serve({
-  fetch: app.fetch,
-  port,
-});
+try {
+  serve({
+    fetch: app.fetch,
+    port,
+  });
+} catch (err) {
+  console.log(`Failed to start the server with error: ${err}`);
+}
